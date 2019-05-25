75d367a64551c489e23451db021614069ccdb4de
Marco <costa.1691388@studenti.uniroma1.it>
2019-05-26 00:35:32 +0200

    Added bottom navbar

--- Android/app/src/main/java/com/example/spidersense/MainActivity.java
+++ Android/app/src/main/java/com/example/spidersense/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.spidersense;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private NavController navController;
+    private BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Getting the Navigation Controller and Bottom Navigation
+        navController = Navigation.findNavController(this, R.id.fragment);
+        bottomNav = findViewById(R.id.bottomNav);
+
+        //Setting the navigation controller to Bottom Nav
+        NavigationUI.setupWithNavController(bottomNav, navController);
+
+        //Setting up the action bar
+        NavigationUI.setupActionBarWithNavController(this, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, (DrawerLayout) null);
     }
 }
