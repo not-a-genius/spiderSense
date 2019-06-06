f81b6dad0e78d62dc2320d9473afc8eebefb4a11
Marco <costa.1691388@studenti.uniroma1.it>
2019-06-06 16:21:14 +0200

    Nucleo sends 2 int, Fixed app behaviour on bluetooth

--- Android/SpiderSense/app/src/main/java/com/iot/spidersense/MainActivity.java
+++ Android/SpiderSense/app/src/main/java/com/iot/spidersense/MainActivity.java
@@ -17,8 +17,10 @@ import android.bluetooth.le.ScanResult;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.preference.PreferenceManager;
@@ -43,6 +45,15 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.listener.single.DialogOnDeniedPermissionListener;
 import com.karumi.dexter.listener.single.PermissionListener;
@@ -230,11 +241,17 @@ public class MainActivity extends AppCompatActivity {
         btManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
         btAdapter = btManager.getAdapter();
         btScanner = btAdapter.getBluetoothLeScanner();
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
         //Enable bluetooth
         if (btAdapter != null && !btAdapter.isEnabled()) {
             Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        }
+
+        //Enable gps
+        if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            enableGPS();
         }
 
         //Processing
@@ -269,6 +286,7 @@ public class MainActivity extends AppCompatActivity {
         AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
+                if(btScanner == null) btScanner = btAdapter.getBluetoothLeScanner();
                 btScanner.startScan(leScanCallback);
             }
         });
@@ -425,5 +443,48 @@ public class MainActivity extends AppCompatActivity {
         if (sketch != null) {
             sketch.onNewIntent(intent);
         }
+    }
+
+    public void enableGPS() {
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+
+        Task<LocationSettingsResponse> result = LocationServices.getSettingsClient(MainActivity.this).checkLocationSettings(builder.build());
+
+        result.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+                try {
+                    LocationSettingsResponse response = task.getResult(ApiException.class);
+                    // All location settings are satisfied. The client can initialize location
+                    // requests here.
+                } catch (ApiException exception) {
+                    switch (exception.getStatusCode()) {
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                            // Location settings are not satisfied. But could be fixed by showing the
+                            // user a dialog.
+                            try {
+                                // Cast to a resolvable exception.
+                                ResolvableApiException resolvable = (ResolvableApiException) exception;
+                                // Show the dialog by calling startResolutionForResult(),
+                                // and check the result in onActivityResult().
+                                resolvable.startResolutionForResult(
+                                        MainActivity.this,
+                                        LocationRequest.PRIORITY_HIGH_ACCURACY);
+                            } catch (IntentSender.SendIntentException e) {
+                                // Ignore the error.
+                            } catch (ClassCastException e) {
+                                // Ignore, should be an impossible error.
+                            }
+                            break;
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            // Location settings are not satisfied. However, we have no way to fix the
+                            // settings so we won't show the dialog.
+                            break;
+                    }
+                }
+            }
+        });
     }
 }
---
+++ FromMbedCompiler/sendAngleAndDistance.cpp
@@ -0,0 +1,163 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2006-2015 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "mbed.h"
+#include "HCSR04.h"
+#include "Servo.h"
+#include "ble/BLE.h"
+#include "ble/services/HeartRateService.h"
+#include "BatteryService.h"
+
+#define LOW 0
+#define HIGH 1
+
+#if !defined(IDB0XA1_D13_PATCH)
+DigitalOut led1(LED1, 1);   // LED conflicts SPI_CLK in case of D13 patch
+#endif  
+
+const static char     DEVICE_NAME[]        = "SpiderSense";
+static const uint16_t uuid16_list[]        = {GattService::UUID_HEART_RATE_SERVICE, GattService::UUID_BATTERY_SERVICE};
+
+static volatile bool  triggerSensorPolling = false;
+
+//Define Serial object
+Serial pc(USBTX, USBRX);
+
+// Defines Tirg and Echo pins of the Ultrasonic Sensor
+HCSR04 sensor = HCSR04(D9, D8);
+
+Servo myServo(D10);
+
+// Define pins for LEDs
+DigitalOut greenLED(D4);
+DigitalOut redLED(D2);
+
+int calculateDistance() {
+    // Function for calculating the distance measured by the Ultrasonic sensor
+    return sensor.distance(CM); //TODO make it long
+}
+
+void disconnectionCallback(const Gap::DisconnectionCallbackParams_t *params) {
+    (void)params;
+    BLE::Instance().gap().startAdvertising(); // restart advertising
+}
+
+void periodicCallback(void) {
+#if !defined(IDB0XA1_D13_PATCH)
+    led1 = !led1; /* Do blinky on LED1 while we're waiting for BLE events */
+#endif
+
+    /* Note that the periodicCallback() executes in interrupt context, so it is safer to do
+     * heavy-weight sensor polling from the main thread. */
+    triggerSensorPolling = true;
+}
+
+void onBleInitError(BLE &ble, ble_error_t error) {
+    (void)ble;
+    (void)error;
+   /* Initialization error handling should go here */
+}
+
+void bleInitComplete(BLE::InitializationCompleteCallbackContext *params) {
+    BLE&        ble   = params->ble;
+    ble_error_t error = params->error;
+
+    if (error != BLE_ERROR_NONE) {
+        onBleInitError(ble, error);
+        return;
+    }
+
+    if (ble.getInstanceID() != BLE::DEFAULT_INSTANCE) {
+        return;
+    }
+
+    ble.gap().onDisconnection(disconnectionCallback);
+
+    /* Setup primary service. */
+    uint8_t distance = 60; // init HRM to 60bps
+    HeartRateService distanceService(ble, distance, HeartRateService::LOCATION_FINGER);
+    
+    uint8_t angle = 50;
+    BatteryService angleService(ble, angle);
+
+    /* Setup advertising. */
+    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED | GapAdvertisingData::LE_GENERAL_DISCOVERABLE);
+    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_16BIT_SERVICE_IDS, (uint8_t *)uuid16_list, sizeof(uuid16_list));
+    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::GENERIC_HEART_RATE_SENSOR);
+    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LOCAL_NAME, (uint8_t *)DEVICE_NAME, sizeof(DEVICE_NAME));
+    ble.gap().setAdvertisingType(GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED);
+    ble.gap().setAdvertisingInterval(50); /* 1000ms */
+    ble.gap().startAdvertising();
+
+    // infinite loop
+    while (true) {
+        // check for trigger from periodicCallback()
+        if (ble.getGapState().connected) {
+
+            // Do blocking calls or whatever is necessary for sensor polling.
+            // In our case, we simply update the HRM measurement.
+            // rotates the servo motor from 15 to 165 degrees
+            for(int i=15; i<=165; i++) {
+                myServo.SetPosition(500 + (i * 12.1212));
+                distance = calculateDistance();
+                angle = i;
+                if(angle < 20) {
+                    greenLED = LOW ;
+                    redLED = HIGH;
+                } else {
+                    redLED = LOW ;
+                    greenLED = HIGH ;
+                }
+                // update bps
+                distanceService.updateHeartRate(distance);
+                angleService.updateBatteryLevel(angle);
+                wait_ms(200);
+            }
+
+        // Repeats the previous lines from 165 to 15 degrees
+            for(int i=165; i>15; i--) {
+                myServo.SetPosition(500 + (i * 12.1212));
+                distance = calculateDistance();
+                angle = i;
+                if(angle < 20) {
+                    greenLED = LOW ;
+                    redLED = HIGH;
+                } else {
+                    redLED = LOW ;
+                    greenLED = HIGH ;
+                }
+                distanceService.updateHeartRate(distance);
+                angleService.updateBatteryLevel(angle);
+                wait_ms(200);
+            }
+            
+
+            
+
+        } else {
+            ble.waitForEvent(); // low power wait for event
+        }
+    }
+}
+
+int main(void) {
+    myServo.Enable(1500,20000);
+    Ticker ticker;
+    ticker.attach(periodicCallback, 1); // blink LED every second
+
+    BLE::Instance().init(bleInitComplete);
+}
+
