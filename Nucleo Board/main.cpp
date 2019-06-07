fd77702dc38d0eba1e8e90f24e6c9f7b3d9a9ec3
Marco <costa.1691388@studenti.uniroma1.it>
2019-06-07 20:34:38 +0200

    Modified code

--- Nucleo Board/main.cpp
+++ Nucleo Board/main.cpp
@@ -5,6 +5,7 @@
 #include "DistanceService.h"
 #include "AngleService.h"
 
+// Values of leds
 #define LOW 0
 #define HIGH 1
 
@@ -15,52 +16,57 @@ DigitalOut led1(LED1, 1);   // LED conflicts SPI_CLK in case of D13 patch
 #define UUID_ANGLE_SERVICE 0x990F
 #define UUID_DISTANCE_SERVICE 0x990D
 
-const static char     DEVICE_NAME[]        = "SpiderSense";
-static const uint16_t uuid16_list[]        = {UUID_DISTANCE_SERVICE, UUID_ANGLE_SERVICE};
+// Name of the device
+const static char DEVICE_NAME[] = "SpiderSense";
+
+// List of services
+static const uint16_t uuid16_list[] = {UUID_DISTANCE_SERVICE, UUID_ANGLE_SERVICE};
 
 static volatile bool  triggerSensorPolling = false;
 static bool increment = true;
-
-//Define Serial object
-Serial pc(USBTX, USBRX);
+static double periodicCallbackTime = 0.2;
 
 // Defines Tirg and Echo pins of the Ultrasonic Sensor
 HCSR04 sensor = HCSR04(D9, D8);
 
+// Define pin for Servo
 Servo myServo(D10);
 
 // Define pins for LEDs
 DigitalOut greenLED(D4);
 DigitalOut redLED(D2);
 
+// Function for calculating the distance measured by the Ultrasonic sensor
 int calculateDistance() {
-    // Function for calculating the distance measured by the Ultrasonic sensor
-    return sensor.distance(CM); //TODO make it long
+    return sensor.distance(CM);
 }
 
+// Callback function on disconnection
 void disconnectionCallback(const Gap::DisconnectionCallbackParams_t *params) {
     (void)params;
+    redLED = LOW;
+    greenLED = LOW;
     BLE::Instance().gap().startAdvertising(); // restart advertising
 }
 
+// Periodic callback function
 void periodicCallback(void) {
-#if !defined(IDB0XA1_D13_PATCH)
-    led1 = !led1; /* Do blinky on LED1 while we're waiting for BLE events */
-#endif
-
-    /* Note that the periodicCallback() executes in interrupt context, so it is safer to do
-     * heavy-weight sensor polling from the main thread. */
+    #if !defined(IDB0XA1_D13_PATCH)
+        led1 = !led1; // Do blinky on LED1 while waiting for BLE events
+    #endif
     triggerSensorPolling = true;
 }
 
+// Function for handling initialization errors
 void onBleInitError(BLE &ble, ble_error_t error) {
     (void)ble;
     (void)error;
-   /* Initialization error handling should go here */
 }
 
+
+// Function for initializing
 void bleInitComplete(BLE::InitializationCompleteCallbackContext *params) {
-    BLE&        ble   = params->ble;
+    BLE& ble = params->ble;
     ble_error_t error = params->error;
 
     if (error != BLE_ERROR_NONE) {
@@ -74,20 +80,19 @@ void bleInitComplete(BLE::InitializationCompleteCallbackContext *params) {
 
     ble.gap().onDisconnection(disconnectionCallback);
 
-    /* Setup primary service. */
-    uint8_t distance = 60; // init HRM to 60bps
+    // Setup distance and angle services
+    uint8_t distance = 60;
     DistanceService distanceService(ble, distance);
     
     uint8_t angle = 15;
     AngleService angleService(ble, angle);
 
-    /* Setup advertising. */
+    // Setup advertising
     ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED | GapAdvertisingData::LE_GENERAL_DISCOVERABLE);
     ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_16BIT_SERVICE_IDS, (uint8_t *)uuid16_list, sizeof(uuid16_list));
-    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::GENERIC_HEART_RATE_SENSOR);
     ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LOCAL_NAME, (uint8_t *)DEVICE_NAME, sizeof(DEVICE_NAME));
     ble.gap().setAdvertisingType(GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED);
-    ble.gap().setAdvertisingInterval(50); /* 1000ms */
+    ble.gap().setAdvertisingInterval(50);
     ble.gap().startAdvertising();
     
     myServo.Enable(1500,20000);
@@ -97,34 +102,32 @@ void bleInitComplete(BLE::InitializationCompleteCallbackContext *params) {
         // check for trigger from periodicCallback()
         if (triggerSensorPolling && ble.getGapState().connected) {
             triggerSensorPolling = false;
-            // Do blocking calls or whatever is necessary for sensor polling.
-            // In our case, we simply update the HRM measurement.
-            // rotates the servo motor from 15 to 165 degrees
-                myServo.SetPosition(500 + (angle * 12.1212));
-                distance = calculateDistance();
-                if(distance < 20) {
-                    greenLED = LOW ;
-                    redLED = HIGH;
-                } else {
-                    redLED = LOW ;
-                    greenLED = HIGH ;
-                }
-                distanceService.updateDistanceValue(distance);
-                angleService.updateAngleValue(angle);
-                if(increment) angle++;
-                else angle--;
-                if(angle == 15) increment = true;
-                else if(angle == 165) increment = false;
+            
+            // rotates the servo motor
+            myServo.SetPosition(500 + (angle * 12.1212));  // It normalize the angle between 0 and 180 degrees
+            distance = calculateDistance();
+            if(distance < 20) {
+                greenLED = LOW ;
+                redLED = HIGH;
+            } else {
+                redLED = LOW ;
+                greenLED = HIGH ;
+            }
+            distanceService.updateDistanceValue(distance);
+            angleService.updateAngleValue(angle);
+            if(increment) angle++;
+            else angle--;
+            if(angle == 15) increment = true;
+            else if(angle == 165) increment = false;
         } else {
-            ble.waitForEvent(); // low power wait for event
+            ble.waitForEvent(); // Low power wait for event
         }
     }
 }
 
 int main(void) {
     Ticker ticker;
-    ticker.attach(periodicCallback, 0.2); // blink LED every second
-
+    ticker.attach(periodicCallback, periodicCallbackTime);
     BLE::Instance().init(bleInitComplete);
 }
 
